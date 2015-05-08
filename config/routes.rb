18c760b17649d06c5ba4042fe5eed4dacc5d524c
CJarrell <colby.jarrell@icloud.com>
2015-05-08 15:05:08 -0400

    Added page banner, nav partial, then styled

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'pages#home'
+
+  get "gallery" => "pages#gallery", as: :gallery
+  get "blog" => "pages#blog", as: :blog
+  get "contact" => "pages#contact", as: :contact
+ 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
